c908263ca360470757ddfff4c435e3c7957b9119
Alexander Herbrich <aherbrich32@gmail.com>
2024-12-04 22:51:33 +0100

    added comments for task 1

--- 2024/day04/solution.py
+++ 2024/day04/solution.py
@@ -8,9 +8,11 @@ class Solution:
         
         res = 0
         for i, j in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+            # for every direction iterate only over valid starting (row, col) for 'X'
             for row in range(max(-3*i, 0), len(grid) + min(0, -3*i)):
                 for col in range(max(-3*j, 0), len(grid[row]) + min(0, -3*j)):
                     if grid[row][col] == 'X':
+                        # retrieve the next 3 characters in that direction (notice we need no bounds check)
                         chars = [grid[row+k*i][col+k*j] for k in range(1,4)]
                         if "".join(chars) == "MAS":
                             res += 1
@@ -41,10 +43,4 @@ class Solution:
                         if grid[row-1][col+1] == 'S' and grid[row+1][col+1] == 'S':
                             res += 1
         
-        print(res)
         return res
-                
-                
-                    
-
-Solution().findXMAS("/Users/aherbrich/src/myprojects/adventofcode24/2024/day04/test.txt")
