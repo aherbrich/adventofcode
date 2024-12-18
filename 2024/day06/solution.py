dab5a7a677015c2224e4130492f181d697df77fc
Alexander Herbrich <aherbrich32@gmail.com>
2024-12-18 21:53:44 +0100

    solution task 1 + 2

--- 2024/day06/solution.py
+++ 2024/day06/solution.py
@@ -1,40 +1,123 @@
+from copy import deepcopy
+
 class Solution:
-    def predictPath(self, filename):
+    def readFile(self, filename):
         grid = []
         with open(filename) as file:
             for line in file:
                 chars = list(line.strip())
                 grid.append(chars)
         
-        # 1. find initial position
-        row, col = 0, 0 
+        return grid
+
+    def findStart(self, grid):
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == '^':
-                    grid[i][j] = '.'
-                    row, col = i, j
-                    break
-                
-        # 2. run through maze until exit and count new fields encountered
-        res = 0
-        direc = [(-1, 0), (0, 1), (1,0), (0, -1)]
-        dir_idx = 0
-        while True:
-            if grid[row][col] == '.':
-                res += 1
-                grid[row][col] = 'X'
+                    return i, j
+        
+    def nextStep(self, row, col, direction, grid):
+        if direction == "up":
+            if row == 0 or grid[row-1][col] != '#':
+                return row - 1, col, direction
+            return self.nextStep(row, col, "right", grid)
+        elif direction == "right":
+            if col == len(grid[row]) - 1 or grid[row][col+1] != '#':
+                return row, col + 1, direction
+            return self.nextStep(row, col, "down", grid)
+        elif direction == "down":
+            if row == len(grid) - 1 or grid[row+1][col] != '#':
+                return row + 1, col, direction
+            return self.nextStep(row, col, "left", grid)
+        elif direction == "left":
+            if col == 0 or grid[row][col-1] != '#':
+                return row, col - 1, direction
+            return self.nextStep(row, col, "up", grid)
+    
+    def outOfBounds(self, row, col, grid):
+        return row < 0 or row >= len(grid) or col < 0 or col >= len(grid[row])
+    
+    def leads_to_loop(self, row, col, direction, grid):
+        tmp_grid = deepcopy(grid)
+        
+        def place_dir_marker(row, col, direction):
+            if direction == "up":
+                tmp_grid[row][col] = '^'
+            elif direction == "right":
+                tmp_grid[row][col] = '>'
+            elif direction == "down":
+                tmp_grid[row][col] = 'v'
+            elif direction == "left":
+                tmp_grid[row][col] = '<'
             
-            x, y = direc[dir_idx]
-            new_row, new_col = row + x, col + y
-            if new_row < 0 or new_row >= len(grid) or new_col < 0 or new_col >= len(grid[col]):
+        while True:
+            place_dir_marker(row, col, direction)
+            
+            row, col, direction = self.nextStep(row, col, direction, tmp_grid)
+            if self.outOfBounds(row, col, tmp_grid):
                 break
             
-            if grid[new_row][new_col] == '#':
-                dir_idx = (dir_idx + 1) % 4
-                continue
+            if direction == "up" and tmp_grid[row][col] == '^':
+                return True
+            if direction == "right" and tmp_grid[row][col] == '>':
+                return True
+            if direction == "down" and tmp_grid[row][col] == 'v':
+                return True
+            if direction == "left" and tmp_grid[row][col] == '<':
+                return True
             
-            row, col = new_row, new_col
+        
+    def solution1(self, filename):
+        grid = self.readFile(filename)
+        
+        row, col = self.findStart(grid)
+        
+        res = 0
+        direction = "up"
+        while True:
+            if grid[row][col] != 'X':
+                res += 1
+            grid[row][col] = 'X'
+            
+            row, col, direction = self.nextStep(row, col, direction, grid)
+            if self.outOfBounds(row, col, grid):
+                break
             
         return res
     
-print(Solution().predictPath("/Users/aherbrich/src/myprojects/adventofcode24/2024/day06/test.txt"))
+    def solution2(self, filename):
+        # pretty inefficient solution, but it works
+        grid = self.readFile(filename)
+        
+        row, col = self.findStart(grid)
+        
+        res = 0
+        direction = "up"
+        while True:
+            # check if we can move in the current direction
+            new_row, new_col, new_direction = self.nextStep(row, col, direction, grid)
+            if self.outOfBounds(new_row, new_col, grid):
+                break
+            
+            # if we have not checked if placing a wall at the new position 
+            # leads to a loop, then do so
+            if grid[new_row][new_col] != 'X':
+                # place, check for loop and unplace a wall at the new position
+                saved = grid[new_row][new_col]
+                grid[new_row][new_col] = '#'
+                if self.leads_to_loop(row, col, direction, grid):
+                    res += 1
+                grid[new_row][new_col] = saved
+            
+            # mark the new position as checked for loops
+            grid[new_row][new_col] = 'X'
+            
+            # actually move to the new position
+            row, col, direction = new_row, new_col, new_direction
+            
+        
+        return res
+
+    
+print(Solution().solution1("/Users/aherbrich/src/myprojects/adventofcode24/2024/day06/test.txt"))
+print(Solution().solution2("/Users/aherbrich/src/myprojects/adventofcode24/2024/day06/test.txt"))
