2805bb60c8431cdf7777e7efc72880d6e000f524
Alexander Herbrich <aherbrich32@gmail.com>
2024-12-21 12:23:41 +0100

    refectoring

--- 2024/day20/solution.py
+++ 2024/day20/solution.py
@@ -28,16 +28,16 @@ class Solution:
         return res
     
     def getHelperMaps(self, grid, start_row, start_col):
-        distance_grid = [row[:] for row in grid]
-        path_grid = [row[:] for row in grid]
+        until_end_map = [row[:] for row in grid]
+        upto_here_map = [row[:] for row in grid]
         path = set()
         
         path_len = self.getPathLength(grid)
         stack = [(start_row, start_col, 0)]
         while stack:
             row, col, time = stack.pop()
-            distance_grid[row][col] = path_len - time
-            path_grid[row][col] = time
+            until_end_map[row][col] = path_len - time
+            upto_here_map[row][col] = time
             path.add((row, col))
             
             for delta_row, delta_col in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
@@ -46,105 +46,42 @@ class Solution:
                 if new_row < 0 or new_row >= len(grid) or new_col < 0 or new_col >= len(grid[0]):
                     continue
                 
-                if distance_grid[new_row][new_col] == "." or distance_grid[new_row][new_col] == "E":
+                if until_end_map[new_row][new_col] == "." or until_end_map[new_row][new_col] == "E":
                     stack.append((new_row, new_col, time + 1))
                     break
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == "#":
-                    distance_grid[row][col] = '-'
-                    path_grid[row][col] = '-'
+                    until_end_map[row][col] = '-'
+                    upto_here_map[row][col] = '-'
                     
-        return distance_grid, path_grid, path
-    
-    def calculateSavedTime(self, grid, path, start_row, start_col, cheats_allowed):
-        tmp_grid = [row[:] for row in grid]
-        saved_times = {}
-        saved_routes = {}
-        
-        stack = [(start_row, start_col, 1, cheats_allowed, set(), (-1, -1), (-1, -1))]
-        path_len = len(path)
-        while stack:
-            row, col, time, cheats_left, seen, begin, end = stack.pop()
-            
-            if grid[row][col] == "E":
-                saved = path_len - time
-                if saved > 0:
-                    if saved not in saved_routes:
-                        saved_routes[saved] = set()
-                    if (begin, end) not in saved_routes[saved]:
-                        saved_times[saved] = saved_times.get(saved, 0) + 1
-                    saved_routes[saved].add((begin, end))
-                continue
-            elif cheats_left < cheats_allowed and (row, col) in path:
-                saved = path[(row, col)] + 1 - time
-                if saved > 0:
-                    if saved not in saved_routes:
-                        saved_routes[saved] = set()
-                    if (begin, end) not in saved_routes[saved]:
-                        saved_times[saved] = saved_times.get(saved, 0) + 1
-                    saved_routes[saved].add((begin, end))
-                continue
-            
-            if (row, col) not in path and cheats_left == 0:
-                continue
-            
-            seen_cpy = seen.copy()
-            seen_cpy.add((row, col))
-            
-            for delta_row, delta_col in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    
-                new_row, new_col = row + delta_row, col + delta_col
-                
-                if new_row < 0 or new_row >= len(tmp_grid) or new_col < 0 or new_col >= len(tmp_grid[0]) or (new_row, new_col) in seen:
-                    continue
-                
-                # 1. just go normal, no cheating
-                if tmp_grid[new_row][new_col] == "." or tmp_grid[new_row][new_col] == "E":
-                    cheats_left_cpy = cheats_left - 1 if grid[row][col] == "#" else cheats_left
-                    end_cpy = (new_row, new_col) if grid[row][col] == "#" else end
-                    stack.append((new_row, new_col, time + 1, cheats_left_cpy, seen_cpy, begin, end_cpy))
-                # # 2. cheating
-                if tmp_grid[new_row][new_col] == "#":
-                    if cheats_left > 0:
-                        begin_cpy = (row, col) if cheats_left == cheats_allowed else begin
-                        stack.append((new_row, new_col, time + 1, cheats_left - 1, seen_cpy, begin_cpy, end))
-            
-            
-        
-        return saved_times
-                
-                
+        return until_end_map, upto_here_map, path
+                 
     def solution(self, filename, max_distance):
         grid = self.readFile(filename)
         
         row, col = self.findStart(grid)
         
         time_without_cheat = self.getPathLength(grid)
-        distance_map, path_map, path = self.getHelperMaps(grid, row, col)
+        until_end, upto_here, path = self.getHelperMaps(grid, row, col)
         
         
-        saved_times = {}
-        for field_1 in path:
-            for field_2 in path:
-                if path_map[field_1[0]][field_1[1]] > path_map[field_2[0]][field_2[1]]:
+        res = 0
+        for row_1, col_1 in path:
+            for row_2, col_2 in path:        
+                if upto_here[row_1][col_1] > upto_here[row_2][col_2]:
                     continue
                 
-                distance = abs(field_1[0] - field_2[0]) + abs(field_1[1] - field_2[1])
+                distance = abs(row_1 - row_2) + abs(col_1 - col_2)
                 if distance <= max_distance:
-                    upto_cheat = path_map[field_1[0]][field_1[1]]
-                    from_cheat = distance_map[field_2[0]][field_2[1]]
+                    upto_cheat = upto_here[row_1][col_1]
+                    from_cheat = until_end[row_2][col_2]
                     time_with_cheat = upto_cheat + from_cheat + distance
                     saved_time = time_without_cheat - time_with_cheat
-                    if saved_time > 0:
-                        saved_times[saved_time] = saved_times.get(saved_time, 0) + 1
-        
-        res = 0           
-        for k in sorted(saved_times.keys(), reverse=True):
-            if k >= 100:
-                res += saved_times[k]
-        
+                    if saved_time >= 100:
+                        res += 1
+                        
         return res
          
     
